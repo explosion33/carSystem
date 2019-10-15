679c2c51507b85dae31074e80151a9bfef36389a
explosion33 <38818379+explosion33@users.noreply.github.com>
2019-10-14 21:05:15 -0700

    created Menu UI, with volume slider and mute button

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame.camera
 from pygame.locals import *
 from random import randint
 from pygame import gfxdraw
+from subprocess import call
 
 
 class button (object):
@@ -102,7 +103,7 @@ class button (object):
         elif self.mode == "image":
             self.surface.fill((0,0,0,0))
             img = pygame.image.load(self.color[self.useColor])
-            img = pygame.transform.scale(img, self.size)
+            img = pygame.transform.smoothscale(img, self.size)
 
             self.surface.blit(img, (0,0))
         
@@ -119,14 +120,18 @@ class button (object):
 
         rect = self.surface.get_rect(topleft=self.pos)
         if rect.colliderect(mouse) and click:
-            if self.state[0] != 2:
+            if self.state[0] != 1:
                 self.state[1] = self.state[0]
-                self.state[0] = 2
+                self.state[0] = 1
 
-                self.useColor = 2
+                self.useColor = 1
                 self.draw()
                 
-                self.function()
+                if self.function:
+                    if self.args:
+                        self.function(args)
+                    else:
+                        self.function()
 
         else:
             if self.state[0] != 0:
@@ -141,6 +146,180 @@ class button (object):
             self.interact(click)
         return (self.surface, self.pos)
 
+class toggleButton(button):
+    def __init__(self, pos, size, color, text, function=None):
+        button.__init__(self, pos, size, color, text, function=None, args=None)
+        self.state = True
+        self.oneCLick = True
+        self.rect = self.surface.get_rect(topleft=self.pos)
+        self.function = function
+
+    def interact(self, click):
+        mouse = pygame.Rect(0, 0, 2, 2)
+        xm, ym = pygame.mouse.get_pos()
+        mouse.center = (xm,ym)
+
+        #rect = self.surface.get_rect(topleft=self.pos)
+
+        if mouse.colliderect(self.rect) and click:
+            if self.oneClick:
+                self.oneClick = False
+                if self.state:
+                    self.state = False
+                    self.useColor = 1
+                else:
+                    self.state = True
+                    self.useColor = 0
+                self.draw()
+                
+                if self.function:
+                    self.function(self.state)
+        if not click:
+            self.oneClick = True
+
+class slider(object):
+    """
+        slider(pos, size, barColor, ballImage, maxMin, start function): creates a slider input and calls a function on slider update with the value as an argument\n
+        pos       : position on screen (x,y)\n
+        size      : (w,h)\n
+        barColor  : color of bar where the ball will slide (r,g,b) or (r,g,b,a)\n
+        ballImage : directory of picture of the slider (normal, pressed)\n
+        maxMin    : maximum and minum values of the slider [min, max] (is inclusive)\n
+        start     : the starting value of the slider, int between min and max\n
+        function  : function to be called when the slider is updated\n
+        """
+    def __init__(self, pos, size, barColor, ballImage, maxMin, start=0, function=None):
+        self.pos = pos
+        self.size = size
+        self.ballImage = ballImage
+        self.use = 0
+        self.min = maxMin[0]
+        self.max = maxMin[1]
+        self.function = function
+        self.enabled = True
+        self.firstTouch = None
+        self.value = self.min
+        self.percent = 0
+        
+        print(start)
+        if start <= self.max and start >= self.min:
+            k = start - self.min
+            k = float(k)/(self.max-self.min)
+            k = k * (self.size[0]-self.size[1])
+            self.xoffset = k
+        else:
+            self.xoffset = 0
+
+        self.bar = pygame.Surface((self.size[0]-10, self.size[1]/2))
+        self.AAfilledRoundedRect(self.bar,self.bar.get_rect(),barColor, radius=0.05)
+
+        self.surface = pygame.Surface(self.size)
+        self.draw()
+
+        img = pygame.image.load(self.ballImage[self.use])
+        img = pygame.transform.smoothscale(img, (self.size[1], self.size[1]))
+        self.rect = img.get_rect()
+
+    def AAfilledRoundedRect(self,surface,rect,color,radius=0.4):
+
+        """
+        AAfilledRoundedRect(surface,rect,color,radius=0.4): draws a rounded, antialiased, rectangle\n
+
+        surface : destination\n
+        rect    : rectangle\n
+        color   : rgb or rgba\n
+        radius  : 0 <= radius <= 1
+        """
+
+        rect         = pygame.Rect(rect)
+        color        = pygame.Color(*color)
+        alpha        = color.a
+        color.a      = 0
+        pos          = rect.topleft
+        rect.topleft = 0,0
+        rectangle    = pygame.Surface(rect.size,pygame.SRCALPHA)
+
+        circle       = pygame.Surface([min(rect.size)*3]*2,pygame.SRCALPHA)
+        pygame.draw.ellipse(circle,(0,0,0),circle.get_rect(),0)
+        circle       = pygame.transform.smoothscale(circle,[int(min(rect.size)*radius)]*2)
+
+        radius              = rectangle.blit(circle,(0,0))
+        radius.bottomright  = rect.bottomright
+        rectangle.blit(circle,radius)
+        radius.topright     = rect.topright
+        rectangle.blit(circle,radius)
+        radius.bottomleft   = rect.bottomleft
+        rectangle.blit(circle,radius)
+
+        rectangle.fill((0,0,0),rect.inflate(-radius.w,0))
+        rectangle.fill((0,0,0),rect.inflate(0,-radius.h))
+
+        rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
+        rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
+        
+        return surface.blit(rectangle,pos)
+
+    def draw(self):
+        """
+        draw(): draws button background and text onto self.surface
+        """
+        self.surface = self.surface.convert_alpha()
+        self.surface.convert_alpha()
+
+        self.surface.fill((0,0,0,0))
+        img = pygame.image.load(self.ballImage[self.use])
+        img = pygame.transform.smoothscale(img, (self.size[1], self.size[1]))
+
+        self.surface.blit(self.bar, (5, self.size[1]/4))
+        self.surface.blit(img, (self.xoffset,0))
+
+    def interact(self, click):
+        """
+        interact(click): handles texture changes and function calling for the button\n
+        click : boolean denoting weather or not the mousebutton is currently down
+        """
+        mouse = pygame.Rect(0, 0, 2, 2)
+        xm, ym = pygame.mouse.get_pos()
+        mouse.center = (xm,ym)
+
+        self.rect.topleft = (self.pos[0] + self.xoffset, self.pos[1])
+
+        if self.rect.colliderect(mouse) and click:
+            self.use = 1
+            if self.firstTouch == None:
+                self.firstTouch = (xm - self.pos[0]) - self.xoffset
+            
+        if not click:
+            self.use = 0
+            self.firstTouch = None
+
+        
+        if self.firstTouch:
+            self.xoffset = xm - self.pos[0] - self.firstTouch
+            if self.xoffset < 0:
+                self.xoffset = 0
+            elif self.xoffset > self.size[0]-self.size[1]:
+                self.xoffset = self.size[0]-self.size[1]
+            self.draw()
+
+            p = float(self.xoffset)/(self.size[0]-self.size[1])
+            self.value = p * (self.max-self.min)
+            self.value += self.min
+            self.value = int(self.value)
+            self.percent = int(p*100)
+            
+            self.function(self.value)
+
+        
+    
+    def loop(self, click):
+        if self.enabled:
+            self.interact(click)
+
+        return (self.surface, self.pos)
+
+        
+        
 
 def getCenterPos(dimensions, screenSize):
     """
@@ -158,6 +337,45 @@ def initSwipe():
 def initSwipe2():
     global swipe2Data
     swipe2Data = [swipe2Data[0] ,True, mouse.centerx]
+
+def AAfilledRoundedRect(surface,rect,color,radius=0.4):
+
+        """
+        AAfilledRoundedRect(surface,rect,color,radius=0.4): draws a rounded, antialiased, rectangle\n
+
+        surface : destination\n
+        rect    : rectangle\n
+        color   : rgb or rgba\n
+        radius  : 0 <= radius <= 1
+        """
+
+        rect         = pygame.Rect(rect)
+        color        = pygame.Color(*color)
+        alpha        = color.a
+        color.a      = 0
+        pos          = rect.topleft
+        rect.topleft = 0,0
+        rectangle    = pygame.Surface(rect.size,pygame.SRCALPHA)
+
+        circle       = pygame.Surface([min(rect.size)*3]*2,pygame.SRCALPHA)
+        pygame.draw.ellipse(circle,(0,0,0),circle.get_rect(),0)
+        circle       = pygame.transform.smoothscale(circle,[int(min(rect.size)*radius)]*2)
+
+        radius              = rectangle.blit(circle,(0,0))
+        radius.bottomright  = rect.bottomright
+        rectangle.blit(circle,radius)
+        radius.topright     = rect.topright
+        rectangle.blit(circle,radius)
+        radius.bottomleft   = rect.bottomleft
+        rectangle.blit(circle,radius)
+
+        rectangle.fill((0,0,0),rect.inflate(-radius.w,0))
+        rectangle.fill((0,0,0),rect.inflate(0,-radius.h))
+
+        rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
+        rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
+        
+        return surface.blit(rectangle,pos)
 
 
 def swipe(btn, swiping, lastPos, mouse, direction="left"):
@@ -227,6 +445,23 @@ def swipe(btn, swiping, lastPos, mouse, direction="left"):
 
     return swiping, lastPos, btn, x, moving
 
+def mute(cond):
+    global volume
+    s = str(volume) + "%"
+    if cond:
+        call(["amixer", "-D", "pulse", "sset", "Master", s])
+    else:
+        call(["amixer", "-D", "pulse", "sset", "Master", "0%"])
+
+def chagneVolume(value):
+    global volume
+    volume = value
+
+def addDebug(*args):
+    global debug
+    for i in args:
+        debug += i
+        debug += ", "
 
 #initialize pygame
 pygame.init()
@@ -245,13 +480,15 @@ camera.start()
 mouse = pygame.Rect(0, 0, 2, 2)
 click = False
 dblClick = False
-bckg = (255,255,255)
+bckg = (24,30,39)
 mode = 'menu'
 subMenu = "main"
 changex = 0
 swipeThreshhold = 4.44
 changing = False
 debug = ""
+volume = 50
+lastVolume = 50
 
 #initialize clock
 clock = pygame.time.Clock()
@@ -260,7 +497,22 @@ dt = 0
 
 #definition of UI componetns
 #buttons
-btn1 = button(getCenterPos((300,100), size), (300,100), ("images/buttons/redN.png","images/buttons/redP.png","images/buttons/redP.png"),("TOUCH", (255,255,255),50,""))
+txtColor = (238,238,238)
+UIDevices = button((20,10), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("Devices", txtColor,30,""))
+UICamera = button((20,90), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("Camera", txtColor,30,""))
+UISettings = button((20,170), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("Settings", txtColor,30,""))
+UIBlank = button((20,250), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("", txtColor,30,""))
+UIBlank2 = button((20,330), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("", txtColor,30,""))
+UIBlank3 = button((20,410), (150,60), ("images/buttons/UIBtn.png","images/buttons/UIBtnPressed.png"),("", txtColor,30,""))
+UIButtons = [UIDevices, UICamera, UISettings, UIBlank, UIBlank2, UIBlank3]
+
+#aduio info
+audioBorder = pygame.Surface((400,460), pygame.SRCALPHA)
+audioBorder.fill((0,0,0,0))
+AAfilledRoundedRect(audioBorder,audioBorder.get_rect(),(33,62,69),0.05)
+
+audioMute = toggleButton((230,420), (40,40), ("images/buttons/volumeOn.png", "images/buttons/volumeMute.png"), ("", txtColor,30,""), mute)
+volumeSlider = slider((280, 420), (320, 30), (238,238,238),("images/buttons/slide.png", "images/buttons/slidePressed.png"), [0,100], 50, chagneVolume)
 
 #swipeBtns
 swipeBtn1 = button((size[0]-80,0), (80,size[1]), ((255,255,0),(255,255,0),(255,128,0)), ("",(0,0,0,0), 1, ""), initSwipe)
@@ -272,10 +524,26 @@ swipe2Data = [swipeBtn2, False, 0]
 font = pygame.font.SysFont("", 20)
 
 def menu(disp):
-
+    global UIButtons
+    global audioBorder
+    global audioMute
+    global volumeSlider
     
     #default menu display
     disp.fill(bckg)
+    
+    for btn in UIButtons:
+        a,b = btn.loop(click)
+        disp.blit(a,b)
+
+    disp.blit(audioBorder, (220,10))
+
+    a,b = audioMute.loop(click)
+    disp.blit(a,b)
+
+    a,b = volumeSlider.loop(click)
+    disp.blit(a,b)
+    
 
     #changing to rear-view camera
     global swipe1Data
@@ -425,6 +693,11 @@ while True:
     if click:
         mouse.center = pygame.mouse.get_pos()
 
+    if audioMute.state:
+        if volume != lastVolume:
+            call(["amixer", "-D", "pulse", "sset", "Master", str(volume) + "%"])
+            lastVolume = volume
+
     #main menu screen
     if mode == 'menu':
         display = menu(pygame.Surface(size))
@@ -438,11 +711,13 @@ while True:
         display.blit(text, (0,0))
 
 
-    debug += str(mode) + str(swipe1Data[1]) + str(swipeBtn1.pos) + str(changing) + str(size[0]/swipeThreshhold)
+    addDebug(str(volume), str(volumeSlider.xoffset), str(volumeSlider.value))
 
-    text = font.render(debug, True, (0,0,0))
+    text = font.render(debug, True, (0,255,0))
     display.blit(text, (0,0))
     debug = ""
+
+    #pygame.draw.rect(display, (255,0,0,125), volumeSlider.rect)
 
     screen.blit(display, (0,0))
     pygame.display.flip() 
